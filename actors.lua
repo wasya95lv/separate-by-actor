6024fc708a7277710e80aaea8f5a6ac0fbd6ae7d
Vasyl Poberezhnyk <vasyl.poberezhnyk@gmail.com>
2019-07-15 02:13:31 +0300

    Added actors.lua and getSeperatedSubtitles() function
    actors.lua - config file with list of actors to export by.
    
    getSeperatedSubtitles — return a table with subtitles divided by actor

---
+++ actors.lua
@@ -0,0 +1,3 @@
+actors = {"Actor 1", "Actor 2", "Actor 3", "Actor 4", "Actor 5"}
+
+return actors
--- separate-by-actor.lua
+++ separate-by-actor.lua
@@ -5,26 +5,31 @@ Omae wa moe shindeiru
 ]]
 
 --
-script_name = "Separate by \"Actor\"";
-script_author = "Regnant";
-script_versin = "0.1beta";
-script_description = "Generate separate srt files based on string \"Actor\"";
+script_name = "Separate by \"Actor\""
+script_author = "Regnant"
+script_versin = "0.1beta"
+script_description = "Generate separate srt files based on string \"Actor\""
 --
 
+
 function main(subtitles)
-    -- body
-    num_lines = subtitles.n;
+    --[[ body
+    num_lines = subtitles.n
     --aegisub.debug.out("Number of rows in ass file: %d",num_lines);
-    for i=1,num_lines,1 do
+    for i=1, num_lines, 1 do
         --aegisub.log("%s \n",subtitles[i].class);
         if subtitles[i].class == "dialogue" and not (subtitles[i].actor==".") then 
             --aegisub.log("%d.\n%s\n\n", i, subtitles[i].text);
             --aegisub.log("Actor: ###%s### \n", subtitles[i].actor);
-            aegisub.log( convertRowToSrt( subtitles[i], i ) );
+            aegisub.log( convertRowToSrt(subtitles[i], i) )
         end
     end
-
+    ]]
+    actrs = include "actors.lua"
+    aegisub.log(actrs[1]);
+    getActorsList()
 end
+
 
 --Cheks if subtitles includes "Actor" field
 function validateInput()
@@ -32,22 +37,69 @@ function validateInput()
 
 end
 
+
 --Converts time from ms to hh:mm:ss,ms format
 function convertTime(input)
-    hh = math.floor(input / (60 * 60 * 1000));
-    mm = math.floor((input / (1000 * 60))) % 60;
-    ss = math.floor((input / 1000)) % 60;
-    ms = input % 1000;
+    local hh = math.floor(input / (60 * 60 * 1000))
+    local mm = math.floor((input / (1000 * 60))) % 60
+    local ss = math.floor((input / 1000)) % 60
+    local ms = input % 1000
 
-    return string.format("%02d:%02d:%02d,%03d", hh, mm, ss, ms);
+    return string.format("%02d:%02d:%02d,%03d", hh, mm, ss, ms)
 end
+
 
 --Converts line from ASS to SRT format
 function convertRowToSrt(row, number) 
-    startTime = convertTime(row.start_time);
-    endTime = convertTime(row.end_time);
-    dialogue = row.text:gsub("\\N","\n"):gsub("{.*}","");
-    return string.format("\n%d\n%s --> %s\n%s\n", number, startTime,endTime,dialogue);
+    local startTime = convertTime(row.start_time)
+    local endTime = convertTime(row.end_time)
+    local dialogue = row.text:gsub("\\N","\n"):gsub("{.*}","")
+    
+    return string.format("\n%d\n%s --> %s\n%s\n", number, startTime,endTime,dialogue)
 end
 
-aegisub.register_macro(script_name, script_description, main--[[, validateInput, is_active_function]]);
+
+function getActorsList()
+    local actors = {}
+    
+    aegisub.log("Actors from config: \n")
+    actors_file = io.open("actors.txt", "r")
+    --local data = actors_file:read("*a")
+        
+    for line in actors_file:lines() do
+        table.insert(actors, line);
+    end
+
+    actors_file:close()
+
+    for i=1, #actors, 1 do
+        aegisub.log("%s \n", actors[i])
+    end
+
+    return actors
+end
+
+
+function getSeperatedSubtitles(subtitles, actors)
+    local subtitlesByActor = {}
+    
+    --Creating subtables for each actor
+    for i in actors do
+        subtitlesByActor[ actors[i] ] = {}
+    end
+
+    --[[Filling subtables of subtitlesByActor with 
+        subtitle rows selcted by actor]]
+    for i = 1, #subtitles, 1 do
+        for actor in actors do
+            if string.find(subtitles[i].actor, actors[actor]) then
+               --table.insert( subtitlesByActor[], [pos,] value )
+            end
+        end
+    end
+
+    return subtitlesByActor
+end
+
+
+aegisub.register_macro(script_name, script_description, main--[[, validateInput, is_active_function]])
